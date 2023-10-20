6a41eb2b7f76126142d0e6ed00d5aba71c0ab8de
Faisallm <faisallawan1997@gmail.com>
2023-10-19 23:14:15 -0700

    State

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
 
 // Content is a different type of component
 
 const Content = () => {
-    // current state of our app
-    const [name, setName] = useState('Faisal');
-
+  // current state of our app
+  const [name, setName] = useState("Faisal");
+  const [count, setCount] = useState(0);
 
   // this will automatically create a function
-//   this function will change the state
-//  and cause the component to re-render with
-// the new state.
+  //   this function will change the state
+  //  and cause the component to re-render with
+  // the new state.
   const handleNameChange = () => {
     const names = ["Faisal", "Lawan", "Muhammad"];
     const int = Math.floor(Math.random() * 3);
@@ -21,18 +21,27 @@ const Content = () => {
   //  the event is saved as e
   // e is used to save the current html element
   // that is being worked on.
-  const handleClick3 = (e) => {
-    console.log(e.target.innerHTML);
+  const handleClick = () => {
+    // cout the value of the current state.
+    setCount(count + 1);
+    console.log(count);
   };
+
+  const handleClick2 = () => {
+    console.log(count);
+  }
 
   return (
     <main>
-        {/* this name variable is a state */}
+      {/* this name variable is a state */}
       <p>Hello {name}!</p>
-      
+
       <button onClick={handleNameChange}>Change the name</button>
+      <button onClick={handleClick}>click it!</button>
+      <button onClick={handleClick2}>click it2!</button>
     </main>
   );
+
 };
 
 export default Content;
